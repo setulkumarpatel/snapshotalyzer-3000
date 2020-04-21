fad94015ada5a58eab2e8469674db9aff060c3b5
setul patel <setulkumarpatel@gmail.com>
2020-04-21 00:56:34 -0500

    Update script to use the click module, and show more information about instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,21 @@
 import boto3
+import click
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
+session = boto3.Session(profile_name='shotty')
 ec2= session.resource('ec2')
 
-for i in ec2.instances.all():
-    print(i)
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print(','.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+
+        return
+
+if __name__ == '__main__':
+    list_instances()
